323b9acacc7f8d8bf1de9223892f190733452a82
CSPriya4043 <cspriya20025@gmail.com>
2022-10-01 11:03:11 +0530

    upload

--- Assignment/C.Akila/odd.py
+++
@@ -1,6 +0,0 @@
-maximum =int(input("enter any maximum value:"))
-number=1
-while number<=maximum:
-    if(number%2!=0):
-        print("{0}".format(number))
-    number = number+1
--- Assignment/C.Shanmuga Priya(Team Leader)/Assignment-1/Range of prime.py
+++
@@ -1,9 +0,0 @@
-lower=int(input("enter 1 value:"))
-upper=int(input("enter 2 value:"))
-for num in range(lower,upper):
-    if num>1:
-      for i in range(2,num):
-          if(num%i)==0:
-             break
-      else:
-       print(num)
